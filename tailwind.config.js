b7408e4e785d9558797fbe33564ef63fd577d453
iago <iagoscippo@gmail.com>
2024-10-25 12:17:16 -0300

    actualizado index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./files/**/*.{html,js,php}"],
+  content: [
+    "./files/**/*.{html,js,php}",
+    "./files/index.php"
+  ],
   theme: {
     extend: {},
   },
